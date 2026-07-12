941749b7c2b4703bc84cc8c83f44bf31f5054c55
Imre Gera <hanziness@windowslive.com>
2026-07-12 15:36:23 +0200

    chore: remove no-op test

--- crates/mddb/src/lib.rs
+++ crates/mddb/src/lib.rs
@@ -8,12 +8,3 @@ pub use error::{Error, Result};
 pub use project::MDDBProject;
 pub use search::{SearchResult, Link, ReachableNode};
 
-#[cfg(test)]
-mod tests {
-    // use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(42, 42);
-    }
-}
